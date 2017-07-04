a31b5ff673b3a92784ea17d4c1057485a04ebbb0
大良神 <hebaoliang@192.168.0.114>
2017-07-04 18:01:52 +0800

    auth

--- src/allsum_bi/main.go
+++ src/allsum_bi/main.go
@@ -4,13 +4,20 @@ import (
 	"allsum_bi/db"
 	_ "allsum_bi/routers"
 	"allsum_bi/services/etl"
+	"common/filter"
+	"common/lib/cache"
+	"common/lib/push"
+	"common/lib/redis"
+	"math/rand"
 	"net/http"
 	"os"
 	"os/signal"
+	"time"
 
 	_ "allsum_bi/models"
 
 	"github.com/astaxie/beego"
+	"github.com/ysqi/tokenauth2beego"
 )
 
 func main() {
@@ -26,6 +33,10 @@ func main() {
 	etl.Start()
 	//	etl.TestETL()
 	go beego.Run()
+
+	//INIT
+	Init()
+
 	signal_f()
 }
 
@@ -38,4 +49,50 @@ func signal_f() {
 	signal.Notify(c, os.Interrupt, os.Kill)
 
 	<-c
+}
+
+func Init() {
+	rand.Seed(time.Now().UnixNano())
+
+	key := beego.AppConfig.String("seed")
+	// init log
+	err := InitLog()
+	if err != nil {
+		beego.Error("init log failed : ", err)
+		return
+	}
+
+	// init tokenauth
+	err = tokenauth2beego.Init(key)
+	if err != nil {
+		beego.Error("init token auth failed : ", err)
+		return
+	}
+	// init redis cache
+	err = cache.Init(key)
+	if err != nil {
+		beego.Error("init cache failed : ", err)
+		return
+	}
+
+	// init redis cache
+	err = redis.Init(key)
+	if err != nil {
+		beego.Error("init redis client failed : ", err)
+		return
+	}
+
+	// init push
+	err = push.Init()
+
+}
+
+func InitLog() (err error) {
+	filter.LoadLogFilter()
+	typ := beego.AppConfig.String("log::type")
+	cons := beego.AppConfig.String("log::params")
+	err = beego.SetLogger(typ, cons)
+	beego.SetLogFuncCall(true)
+	//beego.BeeLogger.SetLogFuncCallDepth(4)
+	return
 }
